cc663960f1c7478cddce39ee6e97cddc7d1977c0
Taha Sayyed <codetaha786@gmail.com>
2026-04-26 00:20:40 +0530

    refactor: clean up server.js by removing extra newlines and organizing imports

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,25 @@ import { connectDB } from "../backend/lib/db.js"
 import { ENV } from './lib/env.js'
 import cookieParser from 'cookie-parser'
 import path from "path"
-
-
 import dns from "node:dns/promises";
 dns.setServers(["1.1.1.1"]);
 
-
 const app = express();
 const PORT = ENV.PORT || 5000;
+
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+/**
+ * It parses cookies sent by the browser
+ * And adds them to req.cookies
+ */
+app.use(cookieParser());
+
+//Routes
+
+
+
 
 app.listen(PORT, () => {
     console.log("Server is running on port 5000 ");
