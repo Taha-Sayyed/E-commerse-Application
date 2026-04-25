be1a4ab31c897948a0148f9069ae921b8c04dd35
Taha Sayyed <codetaha786@gmail.com>
2026-04-26 00:33:36 +0530

    feat: add UPSTASH_REDIS_URL to environment configuration and implement Redis connection

--- backend/lib/env.js
+++ backend/lib/env.js
@@ -5,5 +5,6 @@ dotenv.config({ quiet: true });
 export const ENV = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
-  NODE_ENV:process.env.NODE_ENV
+  NODE_ENV:process.env.NODE_ENV,
+  UPSTASH_REDIS_URL:process.env.UPSTASH_REDIS_URL
 };
