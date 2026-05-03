e461d50381090792ced72674590e3e33f96de66f
Taha Sayyed <codetaha786@gmail.com>
2026-05-03 23:30:22 +0530

    feat: add validation schemas for payment processing routes

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { createCheckoutSession, checkoutSuccess } from "../controllers/payment.controller.js";
+import { validate } from "../middleware/validate.js"
+import { createCheckoutSchema, checkoutSuccessSchema } from "../validations/payment.validation.js"
+
 
 const router = express.Router();
 
-router.post("/create-checkout-session", protectRoute, createCheckoutSession);
-router.post("/checkout-success", protectRoute, checkoutSuccess);
+router.post("/create-checkout-session", protectRoute, validate(createCheckoutSchema), createCheckoutSession);
+router.post("/checkout-success", protectRoute, validate(checkoutSuccessSchema), checkoutSuccess);
 
 export default router;
