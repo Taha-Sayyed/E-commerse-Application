751505bbb3ce39a5f283daed41ef26868702ff82
Taha Sayyed <codetaha786@gmail.com>
2026-05-04 15:15:57 +0530

    feat: add CSRF token endpoint and integrate CSRF protection into payment routes

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -3,11 +3,16 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import { createCheckoutSession, checkoutSuccess } from "../controllers/payment.controller.js";
 import { validate } from "../middleware/validate.js"
 import { createCheckoutSchema, checkoutSuccessSchema } from "../validations/payment.validation.js"
+import { csrfProtection } from '../middleware/csrf.middleware.js'
 
 
 const router = express.Router();
 
-router.post("/create-checkout-session", protectRoute, validate(createCheckoutSchema), createCheckoutSession);
-router.post("/checkout-success", protectRoute, validate(checkoutSuccessSchema), checkoutSuccess);
+router.get('/csrf-token', csrfProtection, (req, res) => {
+    res.json({ csrfToken: req.csrfToken() });
+});
+
+router.post("/create-checkout-session", protectRoute, csrfProtection, validate(createCheckoutSchema), createCheckoutSession);
+router.post("/checkout-success", protectRoute, csrfProtection, validate(checkoutSuccessSchema), checkoutSuccess);
 
 export default router;
