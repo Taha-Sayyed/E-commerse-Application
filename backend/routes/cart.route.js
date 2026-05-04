e1990825afe7920134b15dd4ae20bba95f0cd79d
Taha Sayyed <codetaha786@gmail.com>
2026-05-04 15:24:46 +0530

    feat: integrate CSRF protection into cart routes

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -3,12 +3,13 @@ import { getCartProducts, addToCart, removeFromCart, updateQuantity } from '../c
 import { protectRoute } from '../middleware/auth.middleware.js'
 import { validate } from "../middleware/validate.js"
 import { addToCartSchema, removeFromCartSchema, updateQuantitySchema, productIdParamSchema } from "../validations/cart.validation.js"
+import { csrfProtection } from "../middleware/csrf.middleware.js"
 
 const router = express.Router();
 
 router.get("/", protectRoute, getCartProducts);
-router.post("/", protectRoute, validate(addToCartSchema), addToCart);
-router.delete("/", protectRoute, validate(removeFromCartSchema), removeFromCart);
-router.put("/:id", protectRoute, validate(productIdParamSchema, "params"), validate(updateQuantitySchema), updateQuantity);
+router.post("/", protectRoute, csrfProtection, validate(addToCartSchema), addToCart);
+router.delete("/", protectRoute, csrfProtection, validate(removeFromCartSchema), removeFromCart);
+router.put("/:id", protectRoute, csrfProtection, validate(productIdParamSchema, "params"), validate(updateQuantitySchema), updateQuantity);
 
 export default router;
